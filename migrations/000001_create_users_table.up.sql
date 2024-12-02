fce0d25795bba1f02eb8c72f00123bf4a69807c0
Lesion45 <EgorKulagin2005@yandex.ru>
2024-12-02 22:26:50 +0300

    feat: add migrations and migrator

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,6 +1,9 @@
+-- +goose Up
+-- +goose StatementBegin
 CREATE SCHEMA IF NOT EXISTS orders_schema;
 
 CREATE TABLE IF NOT EXISTS orders_schema.order(
    OrderID VARCHAR(255) PRIMARY KEY,
    Data JSONB
 );
+-- +goose StatementEnd
